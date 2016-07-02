3347f5c43e8249f409d76a2cf64897583f5ddc99
Valeriy Soltan <valeriy.soltan@gmail.com>
2016-07-01 23:08:59 -0400

    Brute-Force solution to problem-2
    brute force recursive fibonacci numbers implementation

---
+++ problem-2/even_fibonacci_numbers.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+class EulerError(Exception):
+  """Base class for exceptions in project Euler."""
+  pass
+
+class FibonacciError(EulerError):
+  """Base class for exceptions in Fibonacci module."""
+  pass
+
+def fibonacci_number(n):
+  """Returns N-th Fibonacci number.
+  
+  For instance, fibonacci_numbers(5) -> 8
+  """
+  if n < 1:
+    raise FibonacciError('Fibonacci numbers are defined for natural numbers only.')
+  elif n == 1 or n == 2:
+    return n
+  return fibonacci_number(n - 1) + fibonacci_number(n - 2)
+
+def fibonacci_numbers_below(high_watermark):
+  """Returns list of all Fibonacci numbers under given number.
+  
+  For instance, fibonacci_numbers_below(10) -> [1, 2, 3, 5, 8]
+  """
+  fib_nums = []
+  fib_index = 1
+  fib = fibonacci_number(fib_index)
+  while fib < high_watermark:
+    fib_nums.append(fib)
+    fib_index += 1
+    fib = fibonacci_number(fib_index)
+  return fib_nums
+
+def sum_of_even_fibonacci_numbers_below(high_watermark):
+  """Returns list of all Fibonacci numbers under given number.
+  
+  For instance, fibonacci_numbers_below(10) -> [1, 2, 3, 5, 8]
+  """
+  fib_nums = fibonacci_numbers_below(high_watermark)
+  return sum([x for x in fib_nums if x % 2 == 0])
+
+if __name__ == '__main__':
+  high_watermark = 4000000
+  print 'Sum of Fibonacci numbers under {}: {}'.format(
+    high_watermark, sum_of_even_fibonacci_numbers_below(high_watermark))
