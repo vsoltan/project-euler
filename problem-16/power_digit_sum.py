2b8199bc13ae6b6eed8d0644c8b5e715f251f1ee
Valeriy Soltan <valeriy.soltan@gmail.com>
2016-12-31 13:23:14 -0500

    Attempts at Optimization
    tried to get rid of list and needless appending. However, turned out
    that original method was faster and less cluttered.

--- problem-16/power_digit_sum.py
+++ problem-16/power_digit_sum.py
@@ -1,18 +1,18 @@
-import math
+
 def Power(num, power):
   return(num ** power)
   
-def getDigits(num):
+def getSumDigits(num):
   x = num
   l = list()
   while(len(l) < len(str(x))):
     digit = num % 10
     l.append(digit)
     num /= 10
-  return l
-    
-def sumList(list):
-  return sum(list)
+  return sum(l)
+
+  
+
 
 
 
