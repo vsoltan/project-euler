871502714e98608fb9bc66237107479af2827563
Valeriy Soltan <valeriy.soltan@gmail.com>
2016-12-31 12:24:47 -0500

    Updated problem 1 to a an OOP structure

--- problem-1/multiples_3_and_5_test.py
+++ problem-1/multiples_3_and_5_test.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python
 
 import unittest
-from multiples_3_and_5 import multiples_of_3_and_5_below
-from multiples_3_and_5 import sum_of_multiples_of_3_and_5_below
+from multiples_3_and_5 import MultiplesOf3And5
 
 class TestStringMethods(unittest.TestCase):
   def test_below_1(self):
-    self.assertSetEqual(multiples_of_3_and_5_below(1), set())
+    self.assertSetEqual(MultiplesOf3And5(1).multiples(), set())
 
   def test_below_3(self):
-    self.assertSetEqual(multiples_of_3_and_5_below(3), set())
+    self.assertSetEqual(MultiplesOf3And5(3).multiples(), set())
 
   def test_below_4(self):
-    self.assertSetEqual(multiples_of_3_and_5_below(4), {3})
+    self.assertSetEqual(MultiplesOf3And5(4).multiples(), {3})
 
   def test_below_7(self):
-    self.assertSetEqual(multiples_of_3_and_5_below(7), {3, 5, 6})
+    self.assertSetEqual(MultiplesOf3And5(7).multiples(), {3, 5, 6})
 
   def test_below_10(self):
-    self.assertSetEqual(multiples_of_3_and_5_below(10), {3, 5, 6, 9})
+    self.assertSetEqual(MultiplesOf3And5(10).multiples(), {3, 5, 6, 9})
 
   def test_sum_below_10(self):
-    self.assertEqual(sum_of_multiples_of_3_and_5_below(10), 23)
+    self.assertEqual(MultiplesOf3And5(10).sum_of_multiples(), 23)
 
   def test_below_16(self):
-    self.assertSetEqual(multiples_of_3_and_5_below(16), {3, 5, 6, 9, 10, 12, 15})
+    self.assertSetEqual(MultiplesOf3And5(16).multiples(), {3, 5, 6, 9, 10, 12, 15})
 
   def test_sum_below_1000(self):
-    self.assertEqual(sum_of_multiples_of_3_and_5_below(1000), 233168)
+    self.assertEqual(MultiplesOf3And5(1000).sum_of_multiples(), 233168)
 
 if __name__ == '__main__':
   unittest.main()
