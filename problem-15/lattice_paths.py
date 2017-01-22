043fe5827b06b4c46a5ad0ab58b5d478bf0c474f
Valeriy Soltan <valeriy.soltan@gmail.com>
2017-01-21 23:46:48 -0500

    Refactoring: remove unused code.

--- problem-15/lattice_paths.py
+++ problem-15/lattice_paths.py
@@ -2,15 +2,12 @@
 
 def num_paths(n):
   """Compute number of paths for lattice NxN"""
-  return r_num_paths(n, 0, 0, [])
-
-RIGHT = 'x'
-DOWN = 'y'
+  return r_num_paths(n, 0, 0)
 
 CACHE = {}
 CACHE_HITS = [0]
 
-def r_num_paths(n, x, y, path):
+def r_num_paths(n, x, y):
   """Compute number of paths for lattice NxN starting from point (x, y)"""
   
   cache_key = "n{0}_x{1}_y{2}".format(n, x, y)
@@ -27,10 +24,10 @@ def r_num_paths(n, x, y, path):
   n_paths = 0
   # Current path: add move to the right and compute paths for point (x+1, y)
   if x < n:
-    n_paths += r_num_paths(n, x + 1, y, path[:]+[RIGHT])
+    n_paths += r_num_paths(n, x + 1, y)
   # Current path: add move down and compute paths for point (x, y+1)
   if y < n:
-    n_paths += r_num_paths(n, x, y + 1, path[:]+[DOWN])
+    n_paths += r_num_paths(n, x, y + 1)
 
   # Cache number of paths from point (x, y)
   # print "Path computed: {0} -> {1}".format(cache_key, n_paths)
@@ -39,5 +36,5 @@ def r_num_paths(n, x, y, path):
 
 if __name__ == '__main__':
   n = 20
-  print "Solution: {0}".format(r_num_paths(n, 0, 0, []))
+  print "Solution: {0}".format(r_num_paths(n, 0, 0))
   print "Cache hits: {0}".format(CACHE_HITS[0])
