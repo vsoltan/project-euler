12bbea4e131bdb5846fda08c78535c66b2be0aee
Valeriy Soltan <valeriy.soltan@gmail.com>
2017-01-21 23:12:23 -0500

    Refactoring: return number of paths from point (x, y) from r_num_paths.
    We used to pass the accumulator (num paths) into recursive function,
    but looking to optimize runtime, and cache num paths for point (x, y)
    in a hash table.

--- problem-15/lattice_paths.py
+++ problem-15/lattice_paths.py
@@ -2,33 +2,48 @@
 
 def num_paths(n):
   """Compute number of paths for lattice NxN"""
-  all_paths = []
-  r_num_paths(n, 0, 0, [], all_paths)
-  return all_paths
+  # all_paths = []
+  # all_paths = [0]
+  return r_num_paths(n, 0, 0, [])
+  # return all_paths[0]
 
 RIGHT = 'x'
 DOWN = 'y'
 
-def r_num_paths(n, x, y, path, paths):
+CACHE = {}
+
+def r_num_paths(n, x, y, path):
   """Compute number of paths for lattice NxN starting from point (x, y)"""
-  print "Lattice {2}x{2}; Position (x={0}, y={1})".format(x, y, n)
-  print "Current path: {0}".format(''.join(path))
+  # print "Lattice {2}x{2}; Position (x={0}, y={1})".format(x, y, n)
+  # print "Current path: {0}".format(''.join(path))
+  
+  # cache_key = "n{0}_x{1}_y{2}".format(n, x, y)
+  # if cache_key in CACHE:
+  #   paths += CACHE[cache_key]
+  #   return
 
   # We have reached bottom-right corner: add current path to the list of paths
   # and return
   if x == n and y == n:
-    paths.append(''.join(path))
-    print "All paths: {0}".format('\n'.join(paths))
-    return
+    # paths.append(''.join(path))
+    # paths[0] += 1
+    # print "All paths: {0}".format('\n'.join(paths))
+    # print '.',
+    return 1
 
+  n_paths = 0
   # Current path: add move to the right and compute paths for point (x+1, y)
   if x < n:
-    r_num_paths(n, x + 1, y, path[:]+[RIGHT], paths)
+    n_paths += r_num_paths(n, x + 1, y, path[:]+[RIGHT])
   # Current path: add move down and compute paths for point (x, y+1)
   if y < n:
-    r_num_paths(n, x, y + 1, path[:]+[DOWN], paths)
+    n_paths += r_num_paths(n, x, y + 1, path[:]+[DOWN])
+
+  return n_paths
 
 if __name__ == '__main__':
-  all_paths = []
-  r_num_paths(3, 0, 0, [], all_paths)
-  print "Solution:\n{0}, size: {1}".format('\n'.join(all_paths), len(all_paths))
+  # all_paths = [0]
+  # all_paths = []
+  x = r_num_paths(10, 0, 0, [])
+  # print "Solution:\n{0}, size: {1}".format('\n'.join(all_paths), len(all_paths))
+  print "Solution: {0}".format(x)
--- problem-15/lattice_paths_test.py
+++ problem-15/lattice_paths_test.py
@@ -6,13 +6,22 @@ from lattice_paths import num_paths
 
 class TestLatticePaths(unittest.TestCase):
   def test_paths_1(self):
-    self.assertEquals(2, len(num_paths(1)))
+    self.assertEquals(2, num_paths(1))
 
   def test_paths_2(self):
-    self.assertEquals(6, len(num_paths(2)))
+    self.assertEquals(6, num_paths(2))
 
   def test_paths_3(self):
-    self.assertEquals(20, len(num_paths(3)))
+    self.assertEquals(20, num_paths(3))
+
+  def test_paths_4(self):
+    self.assertEquals(70, num_paths(4))
+
+  def test_paths_5(self):
+    self.assertEquals(252, num_paths(5))
+
+  def test_paths_10(self):
+    self.assertEquals(184756, num_paths(10))
 
 if __name__ == '__main__':
     unittest.main()
