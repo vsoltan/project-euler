8ae1f02e6b161ce2fecab9ed472ab18a39c0de16
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-01-17 15:15:53 -0500

    added main method for easy access to solution

---
+++ problem-12/src/TriangularNumbers.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TriangularNumbers {
+	public static List<Integer> generateFactors(int x) {
+		List<Integer> factors = new ArrayList<Integer>();
+		for (int i = 1; i <= x; ++i) {
+			if (x % i == 0) {
+				factors.add(i);
+			}
+		}
+		return factors;
+	}
+
+	public static long generateNthTriangular(long n) {
+		long triangularSum = 0;
+		for (long i = 1; i <= n; ++i) {
+			triangularSum += i;
+		}
+		return triangularSum;
+	}
+
+}
---
+++ problem-4/test/PalindromicTest.java
@@ -0,0 +1,68 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PalindromicTest {
+
+	@Test
+	public void zeroShouldBePalindromic() {
+		assertTrue(Palindromic.isPalindromic(0));
+	}
+
+	@Test
+	public void evenDigitsShouldBePalindromic() {
+		assertTrue(Palindromic.isPalindromic(11));
+	}
+
+	@Test
+	public void oddDigitsShouldBePalindromic() {
+		assertTrue(Palindromic.isPalindromic(232));
+	}
+
+	@Test
+	public void longOddPalindromic() {
+		assertTrue(Palindromic.isPalindromic(21312));
+	}
+
+	@Test
+	public void evenDigitsNonPalindromic() {
+		assertFalse(Palindromic.isPalindromic(23));
+	}
+
+	@Test
+	public void oddDigitsNonPalindromic() {
+		assertFalse(Palindromic.isPalindromic(231));
+	}
+
+	@Test
+	public void longOddNonPalindromic() {
+		assertFalse(Palindromic.isPalindromic(1213321));
+	}
+
+	@Test
+	public void longEvenNonPalindromic() {
+		assertTrue(Palindromic.isPalindromic(12133121));
+	}
+
+	@Test
+	public void largest1DigitNumber() {
+		assertEquals(9, Palindromic.largestNthDigitNumber(1));
+	}
+
+	@Test
+	public void largest2DigitNumber() {
+		assertEquals(99, Palindromic.largestNthDigitNumber(2));
+	}
+
+	@Test
+	public void largest3DigitNumber() {
+		assertEquals(999, Palindromic.largestNthDigitNumber(3));
+	}
+
+	@Test
+	public void largestPalindromProductOfTwo2DigitNumbers() {
+		assertEquals(9009, Palindromic.largestPalindromProductOfTwoNDigitNumbers(2));
+	}
+
+}
